68c980ee473689dd5cf68f0f9ac543e612d1c4e1
Hadi Yazdani <hadi.yazdani@protonmail.com>
2024-10-08 20:35:24 +0330

    Migrated From SQLite to PostgreSQL

--- Azonix/settings.py
+++ Azonix/settings.py
@@ -87,10 +87,15 @@ WSGI_APPLICATION = 'Azonix.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'azonix',  # Replace with your PostgreSQL database name
+        'USER': 'admin-azonix',  # Replace with your PostgreSQL user
+        'PASSWORD': 'admin',  # Replace with your PostgreSQL password
+        'HOST': 'localhost',  # Use your DB host, 'localhost' for local development
+        'PORT': '5432',  # Default PostgreSQL port
     }
 }
+
 
 
 
